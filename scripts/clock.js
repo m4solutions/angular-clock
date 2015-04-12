ac5b4fd17c05328f54558a1e227443b58055c6b3
Marcus Sanchez <m4rkuz@gmail.com>
2015-04-12 03:00:05 +0000

    init

---
+++ scripts/clock.js
@@ -0,0 +1,11 @@
+// MODULE
+var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource']);
+"use strict";
+
+
+// CONTROLLERS
+angularApp.controller('mainController', ['$scope', '$log', '$filter', '$resource', '$http', function ($scope, $log, $filter, $resource, $http) {
+
+
+
+}]);
