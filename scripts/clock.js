a0e5ff409cf1c95e66d232caea9f86a23e315664
Marcus Sanchez <m4rkuz@gmail.com>
2015-04-13 01:06:44 +0000

    Setting Up of jdigiclock

--- scripts/clock.js
+++ scripts/clock.js
@@ -4,8 +4,26 @@ var angularApp = angular.module('angularApp', ['ngMessages', 'ngResource']);
 
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$log', '$filter', '$resource', '$http', function ($scope, $log, $filter, $resource, $http) {
+angularApp.controller('mainController', ['$scope', '$log', '$filter', '$resource', '$http',
+                                         function ($scope, $log, $filter, $resource, $http) {
+                                           
+    var dateTimeEndpoint =  'http://time.jsontest.com/';                                
 
-
+    $http.get(dateTimeEndpoint)
+                .success(function (result){
+                      $scope.dateTime = result;
+      					 $log.info('Response from: '+ dateTimeEndpoint);
+      					 $log.info(result);
+                }).error(function (result, status){
+                      $log.error(result);
+      					 $scope.dateTime
+                });
+                                           
+                                           
+    $log.log('started...');
 
 }]);
+
+
+
+
